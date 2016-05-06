50ceac0cdd4770a589827b9fd9224996b37167cc
Ramon Gebben <ramon@ra-ge.net>
2016-05-06 09:30:14 +0200

    pasted in some boilerplate

--- app.js
+++ app.js
@@ -25,6 +25,16 @@ server.route({
   method: 'GET',
   path: '/webhook',
   handler: (request, reply) => {
+    // messaging_events = req.body.entry[0].messaging;
+    //  for (i = 0; i < messaging_events.length; i++) {
+    //    event = req.body.entry[0].messaging[i];
+    //    sender = event.sender.id;
+    //    if (event.message && event.message.text) {
+    //      text = event.message.text;
+    //      // Handle a text message from this sender
+    //    }
+    //  }
+    //  res.sendStatus(200);    
     if (request.query['hub.verify_token'] === 'jemoederisdik') {
       return reply(request.query['hub.challenge']);
     }
